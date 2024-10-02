56bc615397a310f054604a19bf81ed7353fdca1a
Nguyễn Công Huy <142013656+ki11ua26@users.noreply.github.com>
2024-10-02 23:24:10 +0700

    Create SortNumericArray.java
    Question 6.5 submitted

---
+++ lab_01/SortNumericArray.java
@@ -0,0 +1,27 @@
+package lab_01;
+import java.util.Arrays;
+import java.util.Scanner;
+public class SortNumericArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        System.out.print("Enter the array: ");
+        int[] num = new int[n];
+        for (int i = 0; i < n; i++)
+            num[i] = scanner.nextInt();
+        Arrays.sort(num);
+        int totalSum = 0;
+        for (int value : num) {
+            totalSum += value;
+        }
+
+        double average = (double) totalSum / num.length;
+
+        System.out.print("Sorted array:");
+        System.out.println(Arrays.toString(num));
+        System.out.println("Sum of elements: " + totalSum);
+        System.out.println("Average of elements: " + average);
+        scanner.close();
+    }
+}
