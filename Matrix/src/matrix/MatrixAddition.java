52708ce0c85d5a185a6a888df19fc59acb2a186b
Nguyễn Công Huy <142013656+ki11ua26@users.noreply.github.com>
2024-10-03 11:25:58 +0700

    Complete assignment 6.6

---
+++ Matrix/src/matrix/MatrixAddition.java
@@ -0,0 +1,33 @@
+package matrix;
+
+public class MatrixAddition{
+    protected static float[][] a = {{1,2},{3,4}}, b = {{4,1},{3,2}};
+    public static float[][] add(float[][] a, float[][] b) {
+		float[][] ret = {{}};
+		if (a.length != b.length) {
+			return ret;
+		}
+		for (int i = 0; i < a.length; i++) {
+			if (a[i].length != b[i].length) {
+				return ret;
+			}
+		}
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				a[i][j] += b[i][j];
+			}
+		}
+		return a;
+	}
+	
+	public static void main(String[] args) {
+		a = add(a, b);
+		for (float[] i : a) {
+			for (float j : i) {
+				System.out.print(String.format("%.2f ", j));
+			}
+			System.out.println();
+		}
+	}
+
+}
