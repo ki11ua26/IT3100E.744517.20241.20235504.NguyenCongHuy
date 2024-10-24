940d6d17ebd27ca122c6b249492dc14a3e00396c
Nguyễn Công Huy <142013656+ki11ua26@users.noreply.github.com>
2024-10-24 17:25:59 +0700

    first submission(2)

--- lab02/AimsProject/src/aimsprj/Aims.java
+++ lab02/AimsProject/src/aimsprj/Aims.java
@@ -1,5 +1,5 @@
 package aimsprj;
 
 public class Aims {
-
+	
 }
--- lab02/AimsProject/src/aimsprj/Cart.java
+++ lab02/AimsProject/src/aimsprj/Cart.java
@@ -1,5 +1,7 @@
 package aimsprj;
 
 public class Cart {
-
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	public int qtyOrdered;
 }
