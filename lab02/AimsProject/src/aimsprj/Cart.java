a5147729436f3f88b906cc565b520e6af7f192cf
Nguyễn Công Huy <142013656+ki11ua26@users.noreply.github.com>
2024-10-29 21:15:14 +0700

    Final submission

--- lab02/AimsProject/src/aimsprj/Cart.java
+++ lab02/AimsProject/src/aimsprj/Cart.java
@@ -1,7 +1,41 @@
 package aimsprj;
-
+import java.util.Arrays;
 public class Cart {
 	public static final int MAX_NUMBERS_ORDERED = 20;
 	private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
 	public int qtyOrdered;
+	public boolean addDigitalVideoDisc(DigitalVideoDisc disc) {
+		if (this.qtyOrdered == Cart.MAX_NUMBERS_ORDERED) {
+			System.out.println("Cart is already full, cannot add more");
+			return false;
+		}
+		if (Arrays.asList(itemsOrdered).contains(disc)) {
+			System.out.println("Item's already in the cart");
+			return false;
+		}
+		this.itemsOrdered[this.qtyOrdered] = disc;
+		this.qtyOrdered += 1;
+		System.out.println("Item " + this.qtyOrdered  + " is added");
+		return true;
+	}
+	public boolean removeDigitalVideoDisc(DigitalVideoDisc disc) {
+		if(! Arrays.asList(itemsOrdered).contains(disc)) {
+			System.out.println("Item's not found");
+			return false;
+		}
+		int index = Arrays.asList(this.itemsOrdered).indexOf(disc);
+		for (int i = index; i < this.qtyOrdered - 1; i++) {
+			this.itemsOrdered[i] = this.itemsOrdered[i+1];
+		}
+		this.qtyOrdered -= 1;
+		System.out.println("Item is removed successfully");
+		return true;		
+	}
+	public float totalCost() {
+		float sum = 0;
+		for(int i = 0; i < this.qtyOrdered; i++) {
+			sum += this.itemsOrdered[i].getCost();
+		}
+		return sum;
+	}
 }
