5dde6b7ce9fc33241826cb3059120bc9dc5f1ce6
Nguyễn Công Huy <142013656+ki11ua26@users.noreply.github.com>
2024-12-02 08:49:07 +0700

    Update Track.java

--- AimsProject/src/hust/soict/dsai/aims/media/Track.java
+++ AimsProject/src/hust/soict/dsai/aims/media/Track.java
@@ -1,9 +1,26 @@
 package hust.soict.dsai.aims.media;
 
-public class Track {
+public class Track extends Disc{
+	private String title;
+	private int length;
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public int getLength() {
+		return this.length;
+	}
 
 	public Track() {
 		// TODO Auto-generated constructor stub
 	}
 
+	public Track(String title, String category, String director, int length, float cost) {
+		super(title, category, director, length, cost);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	
 }
